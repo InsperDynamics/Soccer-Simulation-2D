e25373827d7749407d9aed02b15e8731bc540a58
MekhyW <mefkhytheroboticist@gmail.com>
2022-05-24 20:35:31 -0300

    aviso de raiz  no start_soccer.sh

--- start_soccer.sh
+++ start_soccer.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #Spawnar server, monitor, e carregar o codigo da nossa equipe
+#Certifique-se de que a o repositorio foi clonado para /home/borg
 
 gnome-terminal --working-directory=/home/borg/Soccer-Simulation-2D/starter-stack -e "python3 server.py"
 gnome-terminal --working-directory=/home/borg/Soccer-Simulation-2D/starter-stack -e "python3 monitor.py"
